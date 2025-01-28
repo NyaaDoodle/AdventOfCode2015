f5c1165ed36c73a9114abb25d782a1365fdfadf6
NyaaDoodle <nyaadoodle@gmail.com>
2025-01-28 23:15:36 +0200

    Day 1 finished

---
+++ Day01/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Day01
+{
+    internal class Program
+    {
+        private const int startingFloor = 0;
+
+        private static void Main()
+        {
+            string input = SharedUtilities.InputFileMethods.ReadInputFile();
+            int buildingFloor = calculateBuildingFloor(input);
+            Console.WriteLine(buildingFloor);
+            int basementDirectionPosition = getPositionWhenGoingToBasement(input);
+            Console.WriteLine(basementDirectionPosition);
+        }
+
+        private static int calculateBuildingFloor(string floorDirections)
+        {
+            int currentFloor = startingFloor;
+
+            foreach (char directionCharacter in floorDirections)
+            {
+                currentFloor = getNextFloorNumber(directionCharacter, currentFloor);
+            }
+
+            return currentFloor;
+        }
+
+        private static int getPositionWhenGoingToBasement(string floorDirections)
+        {
+            const int basementFloor = -1;
+            int currentFloor = startingFloor;
+            int currentDirectionPosition = 0;
+            int atBasementDirectionPosition = -1;
+
+            foreach (char directionCharacter in floorDirections)
+            {
+                currentFloor = getNextFloorNumber(directionCharacter, currentFloor);
+                currentDirectionPosition++;
+                if (currentFloor <= basementFloor)
+                {
+                    atBasementDirectionPosition = currentDirectionPosition;
+                    break;
+                }
+            }
+
+            return atBasementDirectionPosition;
+        }
+
+        private static int getNextFloorNumber(char directionCharacter, int currentFloor)
+        {
+            const char goFloorUpCharacter = '(';
+            const char goFloorDownCharacter = ')';
+            int nextFloor = currentFloor;
+
+            switch (directionCharacter)
+            {
+                case goFloorUpCharacter:
+                    nextFloor++;
+                    break;
+                case goFloorDownCharacter:
+                    nextFloor--;
+                    break;
+            }
+
+            return nextFloor;
+        }
+    }
+}
---
+++ SharedUtilities/InputFileMethods.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedUtilities
+{
+    public class InputFileMethods
+    {
+        public const string InputFileName = "input";
+        public static string ReadInputFile()
+        {
+            return File.ReadAllText(GetInputFilePath());
+        }
+
+        public static string GetProjectRootDirectory()
+        {
+            string currentWorkingDirectory = Environment.CurrentDirectory;
+            return Directory.GetParent(currentWorkingDirectory).Parent.Parent.FullName;
+        }
+
+        public static string GetInputFilePath()
+        {
+            return Path.Join(GetProjectRootDirectory(), InputFileName);
+        }
+    }
+}
